9deec4ce647c2c57a3ea2bd75cb721ca0ec9226c
AnusreeMR <147145346+AnusreeMR@users.noreply.github.com>
2024-04-06 11:43:29 +0530

    Completed

---
+++ Authentication/Authentication.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class InvalidUserException extends Exception{
+public InvalidUserException(String msg){
+super(msg);
+}
+}
+public class Authentication{
+public static void main(String[]args){
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter Username:");
+String username =sc.nextLine();
+System.out.println("Enter Password:");
+String password= sc.nextLine();
+try{
+if(username.length()<8)
+throw new InvalidUserException("Invalid username,username must be greater then 7 characters!🥲️");
+else if(!password.equals("g0dBle2sU"))
+{
+throw new InvalidUserException("Incorrect password,Enter correct password🥲️");
+}
+else{
+System.out.println("Login Succesful😃️");
+}
+}catch(InvalidUserException e){
+e.printStackTrace();
+}
+}
+}
